36aa02b10ac62e64490eaf1171c35e7e4601f755
Artenlaclase <artenlaclase@gmail.com>
2024-12-21 11:13:17 -0300

    enviar correo

---
+++ src/mailer/mailer.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import * as nodemailer from "nodemailer";
+
+@Injectable()
+export class MailerService {
+    private transporter;
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            service: 'gmail',
+
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+    }
+
+    async sendMail(to: string, subject: string, html: string) {
+        try {
+            await this.transporter.sendMail({
+                from: process.env.MAIL_USER,
+                to,
+                subject,
+                html,
+            });
+            console.log(`Email sent to ${to}`);
+        } catch (error) {
+            console.error('Error sent email', error);
+            throw error;
+        }
+    }
+}
