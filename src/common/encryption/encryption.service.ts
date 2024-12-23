80a83df7ffe6c0439df08969e63214faf0449328
Artenlaclase <artenlaclase@gmail.com>
2024-12-22 22:55:08 -0300

    uso de buffer

--- src/common/encryption/encryption.service.ts
+++ src/common/encryption/encryption.service.ts
@@ -3,8 +3,14 @@ import * as crypto from 'crypto';
 
 @Injectable()
 export class EncryptionService {
-  private readonly AES_KEY = process.env.AES_KEY || 'clave_secreta_32_caracteres_123456';
-  private readonly AES_IV = process.env.AES_IV || 'vector_inicial_1234';
+  private readonly AES_KEY = Buffer.from(
+    process.env.AES_KEY || 'clave_secreta_32_caracteres_12345678',
+    'utf8',
+  ); // Clave de 32 bytes
+  private readonly AES_IV = Buffer.from(
+    process.env.AES_IV || 'vector_inicial_1234',
+    'utf8',
+  ); // IV de 16 bytes
 
   encrypt(data: string): string {
     const cipher = crypto.createCipheriv('aes-256-cbc', this.AES_KEY, this.AES_IV);
