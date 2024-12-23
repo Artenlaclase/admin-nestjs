412585dc801c096130fe8c19629158a35be76432
Artenlaclase <artenlaclase@gmail.com>
2024-12-22 22:54:25 -0300

    encriptacion service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, ConflictException } from '@nestjs/common';
-import{InjectRepository} from '@nestjs/typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create.user.dto';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
 import { MailerService } from '../mailer/mailer.service';
 import { generateResetToken } from './utils/token-utils';
+import { EncryptionService } from '../common/encryption/encryption.service';
+import { log } from 'console';
 
 @Injectable()
 export class UsersService {
@@ -13,7 +15,8 @@ export class UsersService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly mailerService: MailerService,
-  ) {}
+    private readonly encryptionService: EncryptionService,
+  ) { }
 
   async createUser(createUserDto: CreateUserDto) {
     const { rut, correo, nombre, rol } = createUserDto;
@@ -36,36 +39,41 @@ export class UsersService {
 
     // Hashear la contraseña
     const hashedPassword = await bcrypt.hash(rawPassword, 10);
+    // Cifrar el RUT y el correo electrónico
+    const encryptedRut = this.encryptionService.encrypt(rut);
+    const encryptedCorreo = this.encryptionService.encrypt(correo);
 
     // Crear y guardar el usuario
     const user = this.userRepository.create({
-      rut,
+      rut: encryptedRut,
       nombre,
-      correo,
+      correo: encryptedCorreo,
       rol,
       password: hashedPassword,
     });
 
     const savedUser = await this.userRepository.save(user);
-//  // Enviar correo con credenciales
-//  const resetUrl = `https://miapp.com/reset-password?token=${resetToken}`;
-//  const emailHtml = `
-//    <h1>Bienvenido, ${nombre}!</h1>
-//    <p>Estas son tus credenciales iniciales:</p>
-//    <p>Contraseña: ${rawPassword}</p>
-//    <p><a href="${resetUrl}">Haz clic aquí para cambiar tu contraseña.</a></p>
-//  `;
 
-//  await this.mailerService.sendMail(correo, 'Bienvenido a la aplicación', emailHtml);
+    //  // Enviar correo con credenciales
+    //  const resetUrl = `https://miapp.com/reset-password?token=${resetToken}`;
+    //  const emailHtml = `
+    //    <h1>Bienvenido, ${nombre}!</h1>
+    //    <p>Estas son tus credenciales iniciales:</p>
+    //    <p>Contraseña: ${rawPassword}</p>
+    //    <p><a href="${resetUrl}">Haz clic aquí para cambiar tu contraseña.</a></p>
+    //  `;
 
- console.log(`Usuario creado con contraseña: ${rawPassword}`);
- return savedUser;
-}
+    //  await this.mailerService.sendMail(correo, 'Bienvenido a la aplicación', emailHtml);
+
+    console.log(`Usuario creado con contraseña: ${rawPassword}`);
+    console.log(encryptedRut, encryptedCorreo);// Ver el RUT y correo cifrados en la consola
+    return savedUser;
+  }
 
 
   getAllUsers() {
     return this.userRepository.find();
-   
+
   }
 
   private generateRandomPassword(): string {
