f4c797634ac0dd82f40089ae8f95183c3b6c5c98
Artenlaclase <artenlaclase@gmail.com>
2024-12-22 23:13:17 -0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,  // Esto hace que las variables de entorno estén disponibles globalmente
+      envFilePath: '.env',  // Este es el archivo donde se leen las variables
+    }),
+    
     TypeOrmModule.forRoot({
       type: 'sqlite', // Cambia esto según tu base de datos (MySQL, PostgreSQL, etc.)
       database: 'db.sqlite',
