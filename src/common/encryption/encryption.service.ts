b8e42ed787feae59699927d4b076d78c1688dbfa
Artenlaclase <artenlaclase@gmail.com>
2024-12-23 00:04:42 -0300

    falta en el encryp

--- src/common/encryption/encryption.service.ts
+++ src/common/encryption/encryption.service.ts
@@ -4,13 +4,18 @@ import * as crypto from 'crypto';
 @Injectable()
 export class EncryptionService {
   private readonly AES_KEY = Buffer.from(
-    process.env.AES_KEY || 'clave_secreta_32_caracteres_12345678',
+    process.env.AES_KEY || 'clave_secreta_32_caracteres_1234',
     'utf8',
   ); // Clave de 32 bytes
   private readonly AES_IV = Buffer.from(
-    process.env.AES_IV || 'vector_inicial_1234',
+    process.env.AES_IV || 'vector_inicia_12',
     'utf8',
   ); // IV de 16 bytes
+
+  constructor() {
+    console.log('AES_IV:', this.AES_IV.toString('utf8')); // Mostrar el IV en UTF-8
+  }
+
 
   encrypt(data: string): string {
     const cipher = crypto.createCipheriv('aes-256-cbc', this.AES_KEY, this.AES_IV);
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log('Length of IV:', Buffer.from('vector_inicia_12', 'utf8').length);
+  console.log('AES_IV:', process.env.AES_IV);
+  console.log('Length of AES_KEY:', Buffer.from('clave_secreta_32_caracteres_1234', 'utf8').length);
+ 
+
 }
 bootstrap();
 dotenv.config();
